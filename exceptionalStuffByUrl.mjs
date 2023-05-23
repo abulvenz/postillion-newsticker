7d64f457b41bad87a0eea9f420aecda0f8d46832
abulvenz <abulvenz@protonmail.ch>
2023-05-23 13:00:59 +0200

    Making puppeteer faster by reducing traffic to 5%.

---
+++ exceptionalStuffByUrl.mjs
@@ -0,0 +1,174 @@
+export const exceptionalStuffByUrl = {
+  "https://www.der-postillon.com/2022/05/newsticker-1795.html": {
+    num: 1796,
+  },
+  "https://www.der-postillon.com/2017/05/newsticker-1053.html": {
+    authors: "marc, evw, rav, tsc, hpa, tom, tei",
+  },
+  "https://www.der-postillon.com/2017/01/newsticker-1003.html": {
+    authors: "tsc, p2k, sen, evw, kol, mag, wwe",
+  },
+  "https://www.der-postillon.com/2017/01/newsticker-1000.html": {
+    tickers: [
+      "Mit Fuchsschwanz abgesägt: Manta-Antenne gestohlen",
+      "Dorffelltester: Stammesfürst darf Tierhäute als Erster anprobieren",
+
+      "Eingemischt: Polier bei Streitschlichtung in Betonmaschine gestürzt",
+      "Hinterlässt verbrannte Erde: Unfähiger Elektriker flüchtet ins Weltall",
+      "Geht abends gern zur Au-Pair: Ehemann täuscht Kulturinteresse vor",
+      "Kriegt kein Auge zu: Leichenpräparator muss die Nacht durcharbeiten",
+      "Lässt ihn einfach stehen: Frau möchte mit Mann nicht mehr über ihren Bart reden",
+
+      "Hat alles probiert: Junkie kommt von keiner einzigen Droge los",
+      "Schuhregal: Schäfer schreinert lieber",
+      "Massiert dort seine Truppen: Feldherr schickt Physiotherapeuten an die Front",
+      "Barbie-Kuh: Ken beschimpft beste Freundin während Grillfest",
+      "Jetzt ist er dran: Mann muss erklären, warum er Anruf zuerst wegdrückte",
+
+      "Folge packt: Neue Serien-Episode mit Action gefüllt",
+      "Zecken-Hemd-Shop: Gebrauchtwarenladen bietet Oberbekleidung für Spinnentiere an",
+      "Frei erfunden: Urlaub gibt es gar nicht",
+      "Darmwinde: Verschluckter Flaschenzug verursacht Blähungen",
+      "Brünett: Kannibalenkoch wünscht braunhaarigem Opfer viel Spaß",
+
+      "Geht immer gleich vor: Konzertbesucher drängelt sich wiederholt in erste Reihe",
+      "Beutelkunst: Känguruh hält Gemälde versteckt",
+      '"Stehenbleiben, Sie sind umzüngelt!": Schlangenbeschwörer stellt Dieb',
+      "Salbei: Arzt behandelt Hodenverletzung mit Kräuterbalsam",
+      "Hat alle berührt: Bewegende Geschichte eines Triebtäters",
+
+      "Kein schönes Bild: Porträtierter vermöbelt Maler",
+      "Hat ihm das Haus zugedacht: Telepath überträgt Sohn sein Erbe",
+      "Falschparka: Politesse verteilt Knöllchen an Modesünder",
+      "Pulli-Zerr-Preis gewonnen: Fotograf erfolgreich am Wühltisch",
+      "Hangar: Pilot grillt Vogel in Flugzeughalle",
+    ],
+    authors:
+      "oga, tsc, kop, wwe, tom, kol, be+, rag, adl, marc, rav, frcx, wye, ber, sta, ban, len, tei, p2k, lou, rot, adg, sod, hpa, evw, rbo, kly,bor/fla/marc,b&b",
+  },
+  "https://www.der-postillon.com/2016/12/newsticker-993.html": {
+    authors: "mpr, mvp, die/adl, hwi, tsc, wwe, oga",
+  },
+  "https://www.der-postillon.com/2015/07/newsticker-774.html": {
+    authors: "sta, daw, rag, rta, wwe, hos, rai/sod",
+    tickers: [
+      "Hatte sich daneben benommen: Hooligan abseits des Stadions erstaunlich nett",
+      "Wachmeister: Polizist gewinnt Nicht-Einschlaf-Wettbewerb",
+      "Sauber getrennt: Deutschlehrerin mit Abschieds-<br>brief von Ehemann zufrieden",
+      "Maribor: Slowenische Zahnarzthelferin zur Arbeit aufgefordert",
+      "Russisch-Rollator: Altenheimbewohner zocken mit nicht bremsbarer Gehhilfe",
+      "Darf Weiter: Anakin Skywalker bei Kontrolle durchgewinkt",
+      "Ist ihm über den Mund gefahren: Lokführer bringt schreienden Selbstmörder zum Schweigen",
+    ],
+  },
+  "https://www.der-postillon.com/2015/01/newsticker-704.html": {
+    authors: "vpe, kop, marc, ban, dtw, sod, gad",
+    tickers: [
+      "Drahtseilakt: Nacktshooting mit Artistin schwieriger als gedacht",
+      "Zauberei: Kaninchen geschlüpft",
+      "Keiner ist auf seiner Seite: Familie rät Opa geschlossen zu Homepage-Löschung",
+      "+++ Weltneuheit: Batteriebetriebener Newsticker - - -",
+      "Alle müssen Opfer bringen: Krankenwagenfahrern steht Urlaubskürzung bevor",
+      "Hochkant: Philosoph wirft lärmende Geburtstagsgäste raus",
+      "Fährt wieder nur zwischen Halle und Hof: Staplerfahrer hat auf Trucker umgeschult      ",
+    ],
+  },
+  "https://www.der-postillon.com/2014/04/newsticker-599.html": {
+    authors: "tim, hg/marc, phi, mlo, tbu, dep, oga",
+    tickers: [
+      "Service wird großgeschrieben: Duden beantwortet Kundenanfrage sofort",
+      "Hautkrebs: Mann lässt Wut über Diagnose an Krustentier aus",
+      "Salmonisches Urteil: Lachs in zwei gleiche Hälften aufgeteilt",
+      "Las sie: Collie entdeckt Leidenschaft zu Büchern",
+      "+++ Voreilig: Schlagzeile war noch gar",
+      "Alteingesessen: 90-Jähriger in seinem Geburtsort verhaftet",
+      "Alles Kopfsache: Henker erläutert mentale Aspekte seiner Arbeit",
+    ],
+  },
+  "https://www.der-postillon.com/2014/01/newsticker-558.html": {
+    authors: "nib, ?, bpy, spu, nan, xyu, cha",
+  },
+  // "https://www.der-postillon.com/2013/09/newsticker-500-xxl-edition-106.html":{
+  //   authors:"sjs,ess,fla,tod,ax,marc,mig,rk,pta,pet,kmü,tim,tim,dne,tim/ste,par",
+  // },
+
+  "https://www.der-postillon.com/2013/09/newsticker-500-xxl-edition-106.html": {
+    authors:
+      "sjs, ess, fla, tod, ax, marc, mig, rk, pta, pet,kmü, tim, tim, dne, tim/ste, par",
+  },
+  "https://www.der-postillon.com/2013/04/newsticker-449.html": {
+    authors: "marc/arne/fis, hel, fla, sdr, rk, rjk, tim",
+    tickers: [
+      "Shitstorm: Internetgemeinde wütend nach Explosion in Klärwerk",
+      "Sind miteinander nicht warm geworden: Kannibalenopfer werden getrennt gekocht",
+      "Beschützerinstinkt: Leibwächterin wehrt Buttersäureangriff ab",
+      "Arbeitet rund um die Uhr: Kirchenrestaurateur vollkommen erschöpft",
+      "Bei Lichte betrachtet: Dark Room ist auch nur ein Zimmer",
+      "Da bleibt kein Auge trocken: Taucher finden Grund zum Lachen",
+      "Wenig Arbeit wegen geringer Qualifikation: Folterknecht kann nur Däumchendrehen",
+    ],
+  },
+
+  "https://www.der-postillon.com/2012/12/newsticker-392.html": {
+    authors: "dlo, frcx, tim, marc, hct, ?, jsm",
+    tickers: [
+      "Knebelvertrag: Pornodarstellerin muss bei Bondagefilmen mitspielen",
+      "Überführt: Autoschieber hatte Kontakte ins Ausland",
+      "Einfach nur krank: Nachlässiger Arzt stellt Pauschaldiagnosen",
+      "Kommt nicht mehr klar: Junkie geht nur noch high zur Arbeit",
+      "Nicht sein Bier: Braumeister ist Konkurrenz egal",
+      "Hunderte Franken verloren: Schweizer Menschenhändler macht großen Verlust",
+      "Was für ein Honk: Mann betätigt wiederholt laute Hupe",
+    ],
+  },
+  "https://www.der-postillon.com/2012/09/newsticker-362.html": {
+    authors: "marc, dav, ?, ste, db, dady, burg",
+  },
+  "https://www.der-postillon.com/2012/06/newsticker-319.html": {
+    authors: "?, ?, marc, ?, ?, evw, ast",
+  },
+  "https://www.der-postillon.com/2012/05/newsticker-314.html": {
+    authors: "bjö, ?, tim, ?, ?, arc, marc",
+  },
+  "https://www.der-postillon.com/2011/07/newsticker-193.html": {
+    tickers: [
+      "Kartograph geil: Weiße Flecken auf der Landkarte",
+      'Fleischersatz: "Tofu ist Scheiße!"',
+      "Manila morgens nass: Sinnloses Anagramm",
+      "++x Doppelpunkt verhaftet Soll Plus umgelegt haben +++",
+      "Knöchel verstaucht: Rockstar kann nicht auftreten",
+      "Steht in der Gegend: Rum",
+    ],
+  },
+  "https://www.der-postillon.com/2011/02/newsticker-143.html": {
+    authors: "ano, lot, svl, tbe, tbe, ucn",
+  },
+  "https://www.der-postillon.com/2010/09/newsticker-100-das-jubilaum.html": {
+    tickers: [],
+    authors: "",
+    num: -1,
+  },
+  "https://www.der-postillon.com/2010/02/vancouver-2010-der-olympia-newsticker.html":
+    {
+      num: "59b",
+    },
+  "https://www.der-postillon.com/2009/03/zeitumstellungsticker.html": {
+    authors: "ssi,ssi,ssi,ssi",
+    num: 9,
+  },
+  "https://www.der-postillon.com/2022/04/muskticker-1.html": {
+    num: 1975,
+  },
+  "https://www.der-postillon.com/2009/04/newsticker-10.html": {
+    tickers: [
+      "Arm: Die Kirchenmäuse des Kölner Doms sammeln für eine Käseglocke",
+      "Schmerzhaft: Matrose verliert Auge durch Mövenpick",
+      "So ein Twixer: Mr. Tom snickert Kit-Kat an den Mars und bekommt voll in die Nuts",
+      "--- Wegen Newsticker: sämtliche Pluszeichen aufgebraucht ---",
+    ],
+  },
+
+  "https://www.der-postillon.com/2009/02/newstickernewstickernewsti.html": {
+    num: 1,
+  },
+};
